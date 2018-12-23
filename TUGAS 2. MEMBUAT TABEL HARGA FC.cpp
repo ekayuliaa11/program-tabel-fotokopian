28f89d213fcc3183ae6291700dfbd030a7a86680
ekayuliaa11 <45647196+ekayuliaa11@users.noreply.github.com>
2018-12-23 13:18:07 +0700

    Add files via upload

---
+++ TUGAS 2. MEMBUAT TABEL HARGA FC.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+
+    int i=1;
+    do{
+        printf("harga%d*Rp.95 = %d\n\n",i,i*95);
+        i++;
+    }
+    while(i<=100);
+    return 0;
+}
